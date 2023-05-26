390f75cc4951bdd8f9e4d3a6b2e1b0c7a78731ad
Haider Abedi <Syed.HaiderAbedi@corp.ibexglobal.com>
2023-05-26 19:48:53 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SidenavComponent } from './sidenav/sidenav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeaderComponent } from './header/header.component';
 import { JobComponent } from './job/job.component';
 import { SummaryComponent } from './summary/summary.component';
 import { CalanderComponent } from './calander/calander.component';
+import { InterviewComponent } from './interview/interview.component';
+import { AssessmentsComponent } from './assessments/assessments.component';
+import { TaskComponent } from './task/task.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { TalentComponent } from './talent/talent.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ConsoleComponent } from './console/console.component';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent, // this is the component with the <router-outlet> in the template
-    // children: [
-    //   {
-    //     path: 'summary', // child route path
-    //     component: , // child route component that the router renders
-    //   },
-    //   {
-    //     path: 'calander',
-    //     component: CalanderComponent, // another child route component that the router renders
-    //   },
-    // ],
-  },
-  { path: 'dashboard/summary', component: SummaryComponent },
-  { path: 'dashboard/calander', component: CalanderComponent },
-  { path: 'hire/engage', component: JobComponent },
+  { path: '', 
+  component: DashboardComponent, 
+  
+ },
+ 
+  { path: 'dashboard/summary', component: SummaryComponent},
+  { path: 'dashboard/calander', component: CalanderComponent,
+  canActivateChild:[AdminGuard] 
+},
+  { path: 'dashboard/interview', component: InterviewComponent },
+  { path: 'dashboard/assessment', component: AssessmentsComponent },
+  { path: 'dashboard/task', component: TaskComponent },
+  { path: 'dashboard/activities', component: ActivitiesComponent },
 
+  
+
+  { path: 'jobs',
+   component: JobComponent,
+   canActivate:[AuthGuard],
+  },
+  { path: 'candidates', component: CandidatesComponent },
+  { path: 'talent', component: TalentComponent },
+  { path: 'assessment', component: AssessmentsComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+  { path: 'console', component: ConsoleComponent },
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,90 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidenavComponent } from './sidenav/sidenav.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { TalentComponent } from './talent/talent.component';
+import { AssessmentsComponent } from './assessments/assessments.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ConsoleComponent } from './console/console.component';
 import { JobComponent } from './job/job.component';
 import { SummaryComponent } from './summary/summary.component';
+import { InterviewComponent } from './interview/interview.component';
+import { TaskComponent } from './task/task.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { CalanderComponent } from './calander/calander.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent,
     HeaderComponent,
     DashboardComponent,
+    SidenavComponent,
+    CandidatesComponent,
+    TalentComponent,
+    AssessmentsComponent,
+    AnalyticsComponent,
+    ConsoleComponent,
     JobComponent,
     SummaryComponent,
-    CalanderComponent
+    CalanderComponent,
+    InterviewComponent,
+    TaskComponent,
+    ActivitiesComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    // * MATERIAL IMPORTS
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    FontAwesomeModule,
     RouterModule,
-    AppRoutingModule
+    ReactiveFormsModule
+    
     
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthGuard , AdminGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
